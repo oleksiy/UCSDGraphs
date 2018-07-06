7fd745b215d2041597cf43be6c52d42a1dc5e8bd
Oleksiy Blavat <blavat.alex@gmail.com>
2018-07-06 00:52:50 -0400

    node class

---
+++ src/roadgraph/Node.java
@@ -0,0 +1,37 @@
+package roadgraph;
+
+import geography.GeographicPoint;
+
+public class Node {
+    public GeographicPoint location;
+    public boolean visited;
+
+    public Node() {
+        this.location = null;
+        this.visited = false;
+    }
+
+    public Node(GeographicPoint location) {
+        this.location = location;
+        this.visited = false;
+    }
+
+    public GeographicPoint getLocation() {
+        return this.location;
+    }
+
+    public boolean isVisited() {
+        return this.visited;
+    }
+
+    @Override
+    public String toString(){
+        return "Node: " + " (" + location.getX() + "," + location.getY() + ")";
+    }
+
+    /**
+    public static void main(String[]args) {
+        Node n = new Node(new GeographicPoint(4.5, 8.5));
+        System.out.println(n.toString());
+    }**/
+}
